f7a143a1b1ff3e354d463e984a8e5111405d7119
MathanKumar-1204 <160117742+MathanKumar-1204@users.noreply.github.com>
2025-03-03 22:55:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def run_strategy():
     return jsonify({"message": "Strategy executed successfully"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001,debug=True)
